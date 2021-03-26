8cd6084b441ed0ea3369c22a1e726b57c86cb9fe
HanSangMin <smhan@simmssoft.com>
2021-03-26 15:45:26 +0900

    git init

---
+++ pages/index.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
+
+const App = () => {
+    const [username, setUsername] = useState("");
+    return (
+        <div>
+            <label>
+                username
+                <input value={username} onChange={(e) => setUsername(e.target.value)} />
+            </label>
+            <p>
+                {username} 깃허브 검색하기
+            </p>
+            <Link href={`/users/${username}`}>
+                <a>검색하기</a>
+            </Link>
+        </div>
+    )
+}
+
+
+export default App;
