482062ef98c2a3e70c81cfc56bfd923a09771e37
HanSangMin <smhan@simmssoft.com>
2021-03-31 19:56:06 +0900

    2.6 까지 진행

---
+++ pages/_app.jsx
@@ -0,0 +1,21 @@
+import '../styles/globals.css'
+import Header from "../components/header";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        body {
+          margin: 0;
+          font-family: Noto Sans, Noto Sans KR;
+        }
+      `}
+
+      </style>
+    </>
+  )
+}
+
+export default MyApp
--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -1,99 +1,62 @@
+import Profile from "../../components/Profile";
+import Repositories from "../../components/Repositories"
 import css from "styled-jsx/css";
 import fetch from "isomorphic-unfetch";
+import formatDistance from "date-fns/formatDistance";
 
 
 
 const style = css`
-    .profile-box {
-        width: 25%;
-        max-width: 272px;
-        margin-right: 26px;
-    }
-
-    .propfile-image-wrapper {
-        width: 100%;
-        border: 1px solid #ele4e8;
-    }
-
-    .profile-image-wrapper .profile-image {
-        display: block;
-        width: 100%;   
+    .user-contents-wrapper {
+        display: flex;
+        padding: 20px;
     }
     
-    .profile-username{
-        margin: 0;
-        padding-top: 16px;
-        font-size: 26px;
-    }
-
-    .profile-user-login {
-        margin: 0;
-        font-size: 20px;
-    }
-
-    .profile-user-bio {
-        margin: 0;
-        padding-top: 16px;
-        font-size: 14px;
-
-    }   
 `;
 
-const name = ({ user }) => {
-    if (!user) {
-        return null;
-    }
+const name = ({ user, repos }) => {
+
     return (
-        <>
-            <div className="profile-box">
-                <div className="profile-iamge-wrapper">
-                    <img
-                        className="profile-image"
-                        src={user.avatar_url}
-                        alt={`${user.name} 프로필 이미지`}
-                    />
-                </div>
-                <h2 className="profile-user-name">{user.name}</h2>
-                <p className="profile-user-login">{user.login}</p>
-                <p className="profile-user-bio">{user.bio}</p>
-            </div>
+
+        <div className="user-contents-wrapper">
+            <Profile user={user} />
+            <Repositories user={user} repos={repos} />
             <style jsx>{style}</style>
-        </>
+        </div>
+
+
     )
 
-}
+};
 
 
-const username = ({ user }) => {
-    return (
-        <>
-            {user ? (
-                <div>
-                    <h2>{user.name}</h2>
-                    <p className="user-bio">{user.bio}</p>
-                </div>
-            ) : (
-                <div>유저 정보가 없습니다.</div>
-            )}
-            <style jsx>{style}</style>
-        </>
-    )
-}
 
 export const getServerSideProps = async ({ query }) => {
-    const { name } = query;
+    const { name, page } = query;
     try {
-        const res = await fetch(`https://api.github.com/users/${name}`);
-        if (res.status === 200) {
-            const user = await res.json();
-            return { props: { user } };
+        let user;
+        let repos;
 
+        const userRes = await fetch(`https://api.github.com/users/${name}`);
+        if (userRes.status === 200) {
+            user = await userRes.json();
         }
-        return { props: {} };
+
+        console.log(user.public_repos);
+        const repoRes = await fetch(`https://api.github.com/users/${name}/repos?sort=updated&page=${page}&per_page=10`);
+
+        if (repoRes.status === 200) {
+            repos = await repoRes.json();
+        }
+
+        console.log("hi");
+
+
+        return { props: { user, repos } };
     } catch (e) {
         console.log(e);
         return { props: {} };
     }
-}
+};
 
 export default name;
